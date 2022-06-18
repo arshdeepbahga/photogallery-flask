6b177e1b3052b7d638f2294d5c61e27c013f429b
arshdeep <abm@merklemail.com>
2022-06-18 10:02:28 +0530

    Updated deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 echo "Starting to deploy docker image.."
 DOCKER_IMAGE=asbind/photogalleryflaskapp:latest
 docker pull $DOCKER_IMAGE
-docker ps -q --filter ancestor=$DOCKER_IMAGE | xargs -r docker stop
+docker rm -f $(docker ps -aq)
 docker run -d -p 5000:5000 $DOCKER_IMAGE
